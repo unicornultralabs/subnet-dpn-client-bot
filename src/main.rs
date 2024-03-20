b8a8adf0334d4a1ba2f5a9485f4f7c58713eab77
Phat Luu <yadsendew@protonmail.com>
2024-03-20 17:11:52 +0700

    update bot

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use log::{error, info};
-use reqwest::Client;
 use serde::{Deserialize, Serialize};
 use static_init::dynamic;
 use std::thread;
@@ -10,7 +9,7 @@ async fn main() {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
     // Set the HTTP proxy address and credentials
-    let proxy_address = &APP_CONFIG.proxy_addr;
+    let proxy_addr = &APP_CONFIG.proxy_addr;
     let proxy_acc: Vec<(&str, &str)> = APP_CONFIG
         .proxy_acc
         .iter()
@@ -26,19 +25,8 @@ async fn main() {
         let (proxy_username, proxy_password) = proxy_acc[i].clone();
         tokio::spawn(async move {
             info!("spawned for {}", proxy_username);
-
-            let client = reqwest::Client::builder()
-                .proxy(
-                    reqwest::Proxy::all(proxy_address)
-                        .unwrap()
-                        .basic_auth(&proxy_username, &proxy_password),
-                )
-                .build()
-                .unwrap();
-
             loop {
-                make_request(&client).await;
-
+                make_request(proxy_addr, proxy_username, proxy_password).await;
                 thread::sleep(Duration::from_secs(1));
             }
         });
@@ -48,28 +36,50 @@ async fn main() {
 }
 
 // Function to make an HTTP request using the provided client
-async fn make_request(client: &Client) {
+async fn make_request(proxy_address: &str, username: &str, password: &str) {
     let url = &APP_CONFIG.download_url;
 
-    // Make a GET request
-    match client.get(url).send().await {
-        Ok(rsp) => match rsp.text().await {
-            Ok(content) => {
-                // info!("{}", content);
-                info!("used {} bytes", content.as_bytes().len());
+    match reqwest::Client::builder()
+        .proxy(
+            reqwest::Proxy::all(proxy_address)
+                .unwrap()
+                .basic_auth(&username, &password),
+        )
+        .build()
+    {
+        Ok(client) => {
+            // Make a GET request
+            match client.get(url).send().await {
+                Ok(rsp) => match rsp.text().await {
+                    Ok(content) => {
+                        // info!("{}", content);
+                        info!("used {} bytes", content.as_bytes().len());
+                    }
+                    _ => {}
+                },
+                Err(e) => {
+                    error!("error when making request err={}", e);
+                    if e.to_string().contains("unexpected eof while tunneling") {
+                        error!("maybe proxy server is down :(");
+                    }
+                }
             }
-            Err(_) => {
-                error!("invalid response format");
-            }
-        },
+        }
         Err(e) => {
-            error!("error when making request err={}", e);
+            error!("cannot create client: err={}", e);
         }
     }
 }
 
 #[dynamic]
 pub static APP_CONFIG: AppConfig = {
+    // let args: Vec<String> = env::args().collect();
+    // if args.len() == 0 {
+    //     panic!("no config filename provided");
+    // }
+    // let config_filename = &args[1];
+    // println!("config filename: {}", config_filename);
+
     let mut file = std::fs::File::open("config.yaml").unwrap();
     let mut contents = String::new();
     std::io::Read::read_to_string(&mut file, &mut contents).unwrap();
